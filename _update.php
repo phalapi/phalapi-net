7122e949a06173a4744b05e1872fc5a3ae6d336c
dogstar <chanzonghuang@gmail.com>
2016-05-17 00:05:49 +0800

    up

--- _update.php
+++ _update.php
@@ -2,7 +2,7 @@
 
 $rs = shell_exec("git pull");
 
-file_put_contents('./_udpate.log', date("Y-m-d H:i:s") . "\n" . $rs . "\n\n", FILE_APPEND);
+file_put_contents('./_update.log', date("Y-m-d H:i:s") . "\n" . $rs . "\n\n", FILE_APPEND);
 echo $rs , "\n\n";
 
 echo "ok";
